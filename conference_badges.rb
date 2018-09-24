3d1c4989bf38af87b627879808eed79378997a56
Merline Belizaire <merline@noukod.com>
2018-09-24 13:19:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,14 @@ def assign_rooms(names_array)
   assigned_room_array
 end
 
-def printer(names_array)
-  batch_badge_creator(names_array).each do |e| puts e end
-  assign_rooms(names_array).each do |e| puts e end
+def printer(speakers)
+  # outputs the results of batch_badge_creator, and assign_rooms
+  badge_messages = batch_badge_creator(speakers)
+  badge_messages.each do |message|
+    puts message
+  end
+  room_messages = assign_rooms(speakers)
+  room_messages.each do |message|
+    puts message
+  end
 end
